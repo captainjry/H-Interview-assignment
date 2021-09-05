60162c27eb48d533b5a31ba9650bf448363708ee
Jirayus Vorawan <jirayus.v@beryl8.com>
2021-09-06 00:41:39 +0700

    Add query string logic

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,40 @@
-import express, { Application, Request, Response } from 'express';
-import { Database } from './database';
+import express, { Application, Request, Response } from "express";
+import { Database } from "./database";
 
 export const app: Application = express();
 
-app.get('/', async (req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
-app.get('/vaccine', async (req: Request, res: Response) => {
-    // fix this please
-    const result = Database.find({ vaccineStatus: 'READY' }); // Database Example
-    res.send(result);
+app.get("/vaccine", async (req: Request, res: Response) => {
+  // fix this please
+  let query = Object.keys(req.query).reduce((mappedQuery, key) => {
+    let param = req.query[key];
+    if (key === "doctor") {
+      mappedQuery["assignedDoctor"] = req.query.doctor;
+    } else {
+      mappedQuery[key] = param;
+    }
+
+    return mappedQuery;
+  }, {});
+
+  const result = Database.find(query); // Database Example
+  if (Array.isArray(result) && result.length) {
+    res.status(200).send(result);
+  } else {
+    res.status(404).send("Not found");
+  }
 });
+
+//   let query: any = {};
+//   if (doctor) {
+//     query.assignedDoctor = doctor;
+//   }
+//   if (vaccineStatus) {
+//     query.vaccineStatus = vaccineStatus;
+//   }
+//   if (vaccineTransactionStatus) {
+//     query.vaccineTransactionStatus = vaccineTransactionStatus;
+// }
